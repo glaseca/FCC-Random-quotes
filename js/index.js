f01e3983982efeb63d95ec4f139fafd9d8742874
lchaglla <lchaglla@maubic.es>
2019-10-25 21:41:04 +0200

    Se cambia el api de random quotes

--- js/index.js
+++ js/index.js
@@ -3,16 +3,15 @@ var currentAuthor = "";
 var forTweet = "";
 var getQuote = function (){
     $.ajax({
-      url: "http://quotesondesign.com/wp-json/posts",
+      url: "https://api.quotable.io/random",
       method: "GET",
       data: {"filter[orderby]":"rand","filter[posts_per_page]":1,"callback":""},
       cache: false,
       dataType: "json"
     })
     .done(function(msg) {
-      var regex = /(<([^>]+)>)/ig;
-      currentQuote = msg[0].content.replace(regex, "").trim();
-      currentAuthor = msg[0].title;
+      currentQuote = msg.content;
+      currentAuthor = msg.author;
      $('.box').html("<blockquote class='blockquote'>" + currentQuote + "<footer class='blockquote-footer''>" + currentAuthor + "</footer></blockquote>");
      forTweet = $('#forTweet').html('"' + currentQuote + '" by ' + currentAuthor).text();
     })
